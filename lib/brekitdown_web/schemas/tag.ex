66ded496c927027706e80dad17070fc406957731
Brandon Ramirez <darkrahd@gmail.com>
2026-08-17 10:57:26 -0600

    Adding fields to OpenAPI schemas

--- lib/brekitdown_web/schemas/tag.ex
+++ lib/brekitdown_web/schemas/tag.ex
@@ -9,12 +9,16 @@ defmodule BrekitdownWeb.Schemas.Tag do
     type: :object,
     properties: %{
       reference_xid: %Schema{type: :string, format: :uuid},
-      name: %Schema{type: :string, maxLength: 50}
+      name: %Schema{type: :string, maxLength: 50},
+      inserted_at: %Schema{type: :string, format: :"date-time"},
+      updated_at: %Schema{type: :string, format: :"date-time"}
     },
-    required: [:reference_xid, :name],
+    required: [:reference_xid, :name, :inserted_at, :updated_at],
     example: %{
       reference_xid: "123e4567-e89b-12d3-a456-426614174000",
-      name: "Urgent"
+      name: "Urgent",
+      inserted_at: "2024-01-01T12:00:00Z",
+      updated_at: "2024-01-01T12:00:00Z"
     }
   })
 end
